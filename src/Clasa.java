9b7fcc47c23de7ccdea3df94d91f9bc90ed94f69
Slava <slavaganta@gmail.com>
2016-11-06 17:30:11 +0200

    first commit

---
+++ src/Clasa.java
@@ -0,0 +1,23 @@
+public class Clasa {
+	public static void main(String[] args) {
+		int restaurantRating = 0;
+
+		switch (restaurantRating) {
+
+		    case 1: System.out.println("This restaurant is not my favorite.");
+		      break;
+
+		    case 2: System.out.println("This restaurant is good.");
+		      break;
+
+		    case 3: System.out.println("This restaurant is fantastic!");
+		      break;
+
+		    default: System.out.println("I've never dined at this restaurant.");
+		      break;
+		}
+
+
+		}
+		
+	}
